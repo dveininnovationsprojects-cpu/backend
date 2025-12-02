022ae786aa808e09210502be879b4dda1b36b0ba
navin2065 <navinvms2065@gmail.com>
2025-12-02 14:21:17 +0530

    fafd50n

--- hrm/src/main/java/com/example/hrm/HrmApplication.java
+++ hrm/src/main/java/com/example/hrm/HrmApplication.java
@@ -7,21 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
+
 public class HrmApplication {
-    @GetMapping("/selvamani")
-    public String showselvamani(){
-        return "hello chennai";
-    }
+
     @GetMapping("/Navin")
     public String shownavin() {
         return "Vanakam da mapla navin rmd";
 
     }
-        @GetMapping("/kishore")
-        public String showKishore () {
-            return "hello team mates ";
-        }
+
         public static void main (String[]args){
             SpringApplication.run(HrmApplication.class, args);
         }
