3feda2c4eae616c4dcbaf62a7b1383d7397b16f2
navin2065 <navinvms2065@gmail.com>
2025-12-02 12:19:37 +0530

    Update From Navin

--- hrm/src/main/java/com/example/hrm/HrmApplication.java
+++ hrm/src/main/java/com/example/hrm/HrmApplication.java
@@ -2,10 +2,15 @@ package com.example.hrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HrmApplication {
+    @GetMapping("/Navin")
+        public String shownavin(){
+        return "Vanakam da mapla navin";
 
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(HrmApplication.class, args);
 	}
