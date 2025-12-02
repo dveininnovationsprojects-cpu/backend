40e04736aff8abd22e6e5378dbc4de2950a02d05
dveininnovationsprojects-cpu <dveininnovations.projects@gmail.com>
2025-12-02 12:18:59 +0530

    update

--- hrm/src/main/java/com/example/hrm/HrmApplication.java
+++ hrm/src/main/java/com/example/hrm/HrmApplication.java
@@ -2,9 +2,17 @@ package com.example.hrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HrmApplication {
+
+	@GetMapping("/kishore")
+	public String showKishore(){
+		return "hello team mates ";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmApplication.class, args);
