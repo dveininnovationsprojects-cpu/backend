5ad219337c1a85cccf992a132df4505dd931fec0
dveininnovationsprojects-cpu <dveininnovations.project@gmail.com>
2025-12-02 14:06:53 +0530

    njinjim

--- hrm/src/main/java/com/example/hrm/HrmApplication.java
+++ hrm/src/main/java/com/example/hrm/HrmApplication.java
@@ -2,9 +2,14 @@ package com.example.hrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HrmApplication {
+    @GetMapping("/selvamani")
+    public String showselvamani(){
+        return "hello chennai";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmApplication.class, args);
